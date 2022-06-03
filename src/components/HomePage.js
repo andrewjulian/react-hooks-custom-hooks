a7f1eb1b427b3c93cf43f9946592be632d6afa8f
+ <+>
2022-06-03 11:06:21 -0600

    commit tree 10b76cfbcccd1c63e42e01dc762a12777bee65d8

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 
 function HomePage() {
-  // fetch data for posts
+  //pulls data from the useQuery function
+  const { data: posts, isLoaded } = useQuery("http://localhost:4000/posts");
+
+  //moved to useQuery.js file -------------------------
+  /* // fetch data for posts
   const [isLoaded, setIsLoaded] = useState(false);
   const [posts, setPosts] = useState([]);
 
@@ -15,12 +21,17 @@ function HomePage() {
         setPosts(posts);
         setIsLoaded(true);
       });
-  }, []);
+  }, []);a */
 
-  // set the document title
+  //moved to useDocumentTitle.js file -------------------
+  /*
   useEffect(() => {
     document.title = "Underreacted | Home";
-  }, []);
+  }, []); */
+
+  // set the document title
+
+  useDocumentTitle("Underreacted | Home");
 
   return (
     <>
